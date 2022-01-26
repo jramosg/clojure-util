5b7276524b21b8fd553139f6bfc53124ccda2590
Jon <jramos@ubikare.io>
2022-01-26 10:36:56 +0100

    fn to fetch datomic schema

--- src/datomic_examples/datomic_examples.clj
+++ src/datomic_examples/datomic_examples.clj
@@ -3,13 +3,25 @@
     [datomic.api :as d]
     [clj-time.coerce :as coerce]))
 
-(defn query-with-as-of [conn email date-str]
+(defn query-with-as-of [db email date-str]
   (d/q
     '[:find ?fn
       :in $ ?email
       :where
       [?u :user/email "jramos@ubikare.io"]
       [?u :user/first-name ?fn]]
-    (d/as-of (d/db conn) (java.util.Date. (coerce/to-long date-str)))
+    (d/as-of db (java.util.Date. (coerce/to-long date-str)))
     email))
+
+(defn fetch-schema [db]
+  (d/q
+    '[:find ?attr ?type ?card
+      :where
+      [_ :db.install/attribute ?a]
+      [?a :db/valueType ?t]
+      [?a :db/cardinality ?c]
+      [?a :db/ident ?attr]
+      [?t :db/ident ?type]
+      [?c :db/ident ?card]]
+    db))
 
